6cfa3b1fbc2ab9565ba39407b207a51950c574a4
ssraw88 <112394046+ssraw88@users.noreply.github.com>
2023-02-26 16:30:30 +0530

    Add files via upload

---
+++ List and Tuple Generator.py
@@ -0,0 +1,7 @@
+user_input = input('Enter the numbers: ').split(',')
+
+list_of_user_input = list(user_input)
+tuple_of_user_input = tuple(user_input)
+
+print(list_of_user_input)
+print(tuple_of_user_input)
